354b9b3cca8bc0fb852d792ff3e3ca0ea657eb09
dan kiss and Masen Matthews <masen@>
2018-04-18 09:39:25 -0700

    first passing test, validates presence of artist

--- db/migrate/20180418154409_create_artist.rb
+++ db/migrate/20180418154409_create_artist.rb
@@ -2,7 +2,7 @@ class CreateArtist < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
       t.column :name, :string
-      t.column :genre, :string
+    
       t.timestamp
     end
   end
