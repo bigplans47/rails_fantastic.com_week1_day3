8335b6c4ac3fd269f7d9a3519c1efcfa442fefc3
dan kiss and Masen Matthews <masen@>
2018-04-18 14:47:42 -0700

    extend CRUD functionality for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,50 +1,51 @@
 class AlbumsController < ApplicationController
-  def index
-    @artists = Artist.all
-    render :index
-  end
-
-  def show
-    @artist = Artist.find(params[:id])
-    render :show
-  end
-
   def new
-    @artist = Artist.new
-    render :new
+    @artist = Artist.find(params[:artist_id])
+    @album = @artist.albums.new
   end
-
   def create
-    @artist = Artist.new(artist_params)
-    if @artist.save
-      redirect_to artists_path
+    @artist = Artist.find(params[:artist_id])
+    @album = @artist.albums.new(album_params)
+    if @album.save
+      redirect_to artist_path(@artist)
     else
       render :new
     end
   end
 
-  def edit
-    @artist = Artist.find(params[:id])
-    render :edit
-  end
 
-  def update
-    @artist = Artist.find(params[:id])
-    if @artist.update(artist_params)
-      redirect_to artists_path
-    else
-      render :edit
-    end
-  end
-
+  # def index
+  #   @artists = Artist.all
+  #   render :index
+  # end
+  #
+  # def show
+  #   @artist = Artist.find(params[:id])
+  #   render :show
+  # end
+  #
+  # def edit
+  #   @artist = Artist.find(params[:id])
+  #   render :edit
+  # end
+  #
+  # def update
+  #   @artist = Artist.find(params[:id])
+  #   if @artist.update(artist_params)
+  #     redirect_to artists_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
   def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    redirect_to artists_path
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to artist_path
   end
 
   private
-  def artist_params
-    params.require(:artist).permit(:name, :genre)
+  def album_params
+    params.require(:album).permit(:review, :year, :title)
   end
 end
